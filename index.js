3714e4228de289d85e076416ec67dcd36ecd4c46
Vikash Kumar <vikashkumar.cse19@chitkarauniversity.edu.in>
2021-09-25 21:58:30 +0530

    2nd push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ fetch('https://api.thedogapi.com/v1/breeds').then((response) => {
     return response.json();
 }).then(data => {
     doglist = data;
-    data.map(dog => {
+    data.map((dog, i) => {
         if (dog.id <=10) {
             add_li(dog);
         }
@@ -51,28 +51,56 @@ const add_li = (dog) => {
     dright.appendChild(rfield);
     dright.appendChild(rans);
 
-    rfield.appendChild(create_p('ID'));
-    rfield.appendChild(create_p('Name'));
-    rfield.appendChild(create_p('Weight'));
-    rfield.appendChild(create_p('Height'));
-    rfield.appendChild(create_p('Country Code'));
-    rfield.appendChild(create_p('Bred For'));
-    rfield.appendChild(create_p('Breed Group'));
-    rfield.appendChild(create_p('Life Span'));
-    rfield.appendChild(create_p('Temprament'));
+    if (dog.id !== undefined) {
+        rfield.appendChild(create_p('ID'));
+        rans.appendChild(create_p(dog.id));
+    }
 
+    if (dog.name !== undefined) {
+        rfield.appendChild(create_p('Name'));
+        rans.appendChild(create_p(dog.name));
+    }
+
+    if (dog.weight.metric !== undefined) {
+        rfield.appendChild(create_p('Weight'));
+        rans.appendChild(create_p(dog.weight.metric));
+    }
+
+    if (dog.height.metric !== undefined) {
+        rfield.appendChild(create_p('Height'));
+        rans.appendChild(create_p(dog.height.metric));
+    }
+
+    if (dog.country_code !== undefined) {
+        rfield.appendChild(create_p('Country Code'));
+        rans.appendChild(create_p(dog.country_code));
+    }
+
+    if (dog.bred_for !== undefined) {
+        rfield.appendChild(create_p('Bred For'));
+        rans.appendChild(create_p(dog.bred_for));
+    }
+
+    if (dog.breed_group !== undefined) {
+        rfield.appendChild(create_p('Breed Group'));
+        rans.appendChild(create_p(dog.breed_group));
+    }
+
+    if (dog.life_span !== undefined) {
+        rfield.appendChild(create_p('Life Span'));
+        rans.appendChild(create_p(dog.life_span));
+    }
+
+    if (dog.temperament !== undefined) {
+        rfield.appendChild(create_p('Temprament'));
+        rans.appendChild(create_p(dog.temperament));
+    }
+
+    if (dog.origin !== undefined) {
+        rfield.appendChild(create_p('Origin'));
+        rans.appendChild(create_p(dog.origin));
+    }
     li.appendChild(dright);
-
-    rans.appendChild(create_p(dog.id));
-    rans.appendChild(create_p(dog.name));
-    rans.appendChild(create_p(dog.weight.metric));
-    rans.appendChild(create_p(dog.height.metric));
-    rans.appendChild(create_p(dog.country_code));
-    rans.appendChild(create_p(dog.bred_for));
-    rans.appendChild(create_p(dog.breed_group));
-    rans.appendChild(create_p(dog.life_span));
-    rans.appendChild(create_p(dog.temperament));
-
     document.getElementById('list').appendChild(li);
 }
 
